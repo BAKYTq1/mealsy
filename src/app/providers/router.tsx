481f34b0c9a5bdd295c3fe8abc4a4bf5caaf5ed0
asanbaevasylbek504-png <asanbaevasylbek504@gmail.com>
2026-03-04 15:31:32 +0600

    redactor

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -6,6 +6,7 @@ import { Befstragenow } from '../../pages/components/Katalogresepts/befstragenof
 import { CatalogRecipes } from '../../pages/components/Katalogresepts/catalog/CatalogRecipes'
 import { Profile } from '../../pages/components/profile/Profile'
 import AuthLayout from '../../pages/auth/authlayout/AuthLayout'
+import Redactor from "../../pages/components/profile/redactor/Redactor";
 
 export const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ export const router = createBrowserRouter([
       {
         path: "profile", 
         element: <Profile/>,
+      },
+        {
+        path: "redactor", 
+        element: <Redactor/>,
       },
     ],
   },
